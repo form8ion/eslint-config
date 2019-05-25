2de51681cbbf63e1d63d315ef420efe690dc2070
Matt Travi <programmer@travi.org>
2019-05-24 22:58:00 -0500

    feat(object-curly): overrode airbnb rules

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ module.exports = {
   ],
 
   rules: {
-    'comma-dangle': ['error', 'never']
+    'comma-dangle': ['error', 'never'],
+    'object-curly-spacing': ['error', 'never'],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          multiline: true,
+          consistent: true
+        },
+        ObjectPattern: {
+          multiline: true,
+          consistent: true
+        },
+        ImportDeclaration: {
+          multiline: true,
+          consistent: true,
+          minProperties: 0
+        }
+      }
+    ]
   }
 };
